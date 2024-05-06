8f740ca21c2badad8aa143443d8c65c9aa088926
Bilguun <bilguun1324@gmail.com>
2024-05-06 14:23:10 +1000

    added custom css properties

--- components/peer-group-example.tsx
+++ components/peer-group-example.tsx
@@ -1,11 +1,12 @@
 export const PeerGroupExample = () => {
   return (
     <div className="flex gap-4">
-      <div className="peer p-8 bg-blue-600 group">
+      <div className="peer p-8 bg-blue-600 group/name">
         Hover me
-        <div className=" group-hover:bg-red-600">Group Example</div>
+        <div className="group-hover:bg-red-600">Group Example 1</div>
+        <div className="group-hover/name:bg-green-600">Group Example 2</div>
       </div>
-      <div className="peer-hover:bg-cyan-400 flex items-center justify-center">
+      <div className="peer-hover:bg-cyan-400 flex items-center justify-center p-8">
         Group Example
       </div>
     </div>
